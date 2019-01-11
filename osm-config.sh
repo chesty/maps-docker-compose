23846967f44e0f0afe6b7920b355d7c82400d69b
Michael Chesterton <michael.chesterton@gmail.com>
2019-01-11 12:23:55 +1100

    clean up some environment variables

--- osm-config.sh
+++ osm-config.sh
@@ -1,20 +1,11 @@
 #!/bin/sh
 
-# these will only be set if they aren't already set
 : ${NPROCS:=$(grep -c ^processor /proc/cpuinfo 2>/dev/null || 1)}
-: ${POSTGRES_PASSWORD:=supersecret}
-: ${POSTGRES_HOST:=postgres}
-: ${POSTGRES_USER:=postgres}
-: ${POSTGRES_DB:=gis}
-: ${POSTGRES_PORT:=5432}
-: ${OSM_PBF_URL:=http://download.geofabrik.de/australia-oceania/australia-latest.osm.pbf}
 : ${OSM_PBF:=$(basename "$OSM_PBF_URL")}
 : ${OSM_PBF_BASENAME:=$(basename "$OSM_PBF" .osm.pbf)}
 : ${OSM_OSRM:="$OSM_PBF_BASENAME".osrm}
-: ${OSM2PGSQLCACHE:=1000}
 
-export DATADIR POSTGRES_USER POSTGRES_DB POSTGRES_HOST POSTGRES_PASSWORD NPROCS OSM_PBF OSM_PBF_URL OSM_PBF_BASENAME \
-    OSM_OSRM OSM2PGSQLCACHE
+export NPROCS OSM_PBF OSM_PBF_BASENAME OSM_OSRM
 
 for U in osm osrm postgres root; do
     if id "$U" > /dev/null 2>&1 && [ ! -f ~"$U"/.pgpass ]; then
