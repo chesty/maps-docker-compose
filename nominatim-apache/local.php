3f499291c3b76fdb3b7c952bf59973985f8aade5
Michael Chesterton <michael.chesterton@gmail.com>
2019-01-17 13:06:03 +1100

    change recheck interval to every hour configure Log_file to log to stdout via logpipe

--- nominatim-apache/local.php
+++ nominatim-apache/local.php
@@ -1,13 +1,13 @@
 <?php
 
-@define('CONST_Log_File', '/data/nominatim/nominatim.log');
+@define('CONST_Log_File', '/Nominatim/build/logpipe');
 @define('CONST_Website_BaseURL', '/');
 
 // Replication settings
 @define('CONST_Replication_Url', 'http://download.geofabrik.de/australia-oceania/australia-updates/');
 @define('CONST_Replication_MaxInterval', '86400');
 @define('CONST_Replication_Update_Interval', '86400');  // How often upstream publishes diffs
-@define('CONST_Replication_Recheck_Interval', '86400'); // How long to sleep if no update found yet
+@define('CONST_Replication_Recheck_Interval', '3600'); // How long to sleep if no update found yet
 
 @define('CONST_Default_Language', false);
 // Appearance of the map in the debug interface.
@@ -16,8 +16,6 @@
 @define('CONST_Default_Zoom', 4);
 @define('CONST_Map_Tile_URL', 'http://maps.localnet:8082/osm_tiles/{z}/{x}/{y}.png');
 @define('CONST_Map_Tile_Attribution', 'hammer'); // Set if tile source isn't osm.org
-
-@define('CONST_Log_File', '/data/nominatim/mominatim.log');
 
 // if you put your postgres password here, nominatim-apache doesn't need to share the run volume with postgres
 // @define('CONST_Database_DSN', 'pgsql://postgres:supersecret@postgres/nominatim');
