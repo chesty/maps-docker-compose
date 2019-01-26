f484583f6385634688b376eea21c079e880fe16e
Michael Chesterton <michael.chesterton@gmail.com>
2019-01-26 22:44:30 +1100

    use osm uid instead of osrm for consistancy with other containers

--- osrm-frontend/osrm-frontend.sh
+++ osrm-frontend/osrm-frontend.sh
@@ -4,15 +4,19 @@ if ! hash curl &> /dev/null || ! hash su-exec &> /dev/null; then
 	apk --no-cache add curl su-exec
 fi
 
+if ! id osm &> /dev/null; then
+	adduser -D osm &> /dev/null
+fi
+
 if ! id osrm &> /dev/null; then
 	adduser -D osrm &> /dev/null
 fi
 
 cd /src && \
-	chown -R osrm /src && \
+	chown -R osm /src && \
 
 if [ "$#" -gt 0 ]; then
 	exec "$@"
 fi
 
-exec su-exec osrm npm start
+exec su-exec osm npm start
