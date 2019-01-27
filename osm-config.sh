ceedde8c63303c5a34f986174bd614dbc6b898f1
Michael Chesterton <michael.chesterton@gmail.com>
2019-01-27 23:52:26 +1100

    log changes and a few other tweaks

--- osm-config.sh
+++ osm-config.sh
@@ -13,8 +13,9 @@ log_env () {
     log `env | grep -v PASSWORD`
 }
 
-log "osm-comfig.sh starting"
+log "starting osm-config.sh"
 
+# these will only be set if they aren't already set
 : ${NPROCS:=$(grep -c ^processor /proc/cpuinfo 2>/dev/null || 1)}
 : ${OSM_PBF:=$(basename "$OSM_PBF_URL")}
 : ${OSM_PBF_BASENAME:=$(basename "$OSM_PBF" .osm.pbf)}
@@ -27,17 +28,22 @@ log_env
 for U in osm osrm postgres root; do
     if id "$U" > /dev/null 2>&1 && [ ! -f ~"$U"/.pgpass ]; then
         if [ ! -d ~ ]; then
-            mkdir ~
+            mkdir -p ~
             chown "$U": ~
         fi
-        ( eval cd ~"$U"; touch .pgpass; chown "$U": .pgpass; chmod 600 .pgpass; \
-            echo "$POSTGRES_HOST:$POSTGRES_PORT:*:$POSTGRES_USER:$POSTGRES_PASSWORD" >> .pgpass; )
+        (
+            eval cd ~"$U"
+            touch .pgpass
+            chown "$U": .pgpass
+            chmod 600 .pgpass
+            echo "$POSTGRES_HOST:$POSTGRES_PORT:*:$POSTGRES_USER:$POSTGRES_PASSWORD" >> .pgpass
+        )
     fi
 done
 
 if [ -d /osm-config.d ]; then
     for script in /osm-config.d/*.sh; do
-        log "running $script"
+        log "osm-config.sh running $script"
         . "$script"
     done
 fi
